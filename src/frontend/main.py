c32d48d312507a9e2c9177959b0eb9c7dc0ec795
adikpb <67222969+adikpb@users.noreply.github.com>
2024-02-16 03:35:50 +0530

    update ui to new model

--- src/frontend/main.py
+++ src/frontend/main.py
@@ -26,84 +26,17 @@ class MainView(ft.View):
                             ],
                             expand=True,
                             alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
+                        ),]+
+                        [ft.Row(
                             controls=[
                                 ft.Row(expand=True),
-                                ft.TextField(label="Pregnancies", expand=True),
+                                ft.TextField(label=i, expand=True),
                                 ft.Row(expand=True),
                             ],
                             expand=True,
                             alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
-                            controls=[
-                                ft.Row(expand=True),
-                                ft.TextField(label="Glucose", expand=True),
-                                ft.Row(expand=True),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
-                            controls=[
-                                ft.Row(expand=True),
-                                ft.TextField(label="Blood Pressure", expand=True),
-                                ft.Row(expand=True),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
-                            controls=[
-                                ft.Row(expand=True),
-                                ft.TextField(label="Skin Thickness", expand=True),
-                                ft.Row(expand=True),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
-                            controls=[
-                                ft.Row(expand=True),
-                                ft.TextField(label="Insulin", expand=True),
-                                ft.Row(expand=True),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
-                            controls=[
-                                ft.Row(expand=True),
-                                ft.TextField(label="BMI", expand=True),
-                                ft.Row(expand=True),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
-                            controls=[
-                                ft.Row(expand=True),
-                                ft.TextField(
-                                    label="Diabetes Pedigree Function", expand=True
-                                ),
-                                ft.Row(expand=True),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
-                            controls=[
-                                ft.Row(expand=True),
-                                ft.TextField(
-                                    label="Age", expand=True
-                                ),
-                                ft.Row(expand=True),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                        ),
-                        ft.Row(
+                        ) for i in ("Age", "Hypertension", "Heart Disease",	"Smoking History", "BMI", "HbA1c Level", "Blood Glucose Level")]+
+                        [ft.Row(
                             controls=[
                                 ft.Row(expand=True),
                                 ft.ElevatedButton(
@@ -138,14 +71,14 @@ class MainView(ft.View):
 
     async def post_req(self, e=None):
         json = {
-            "pregnancies": int(self.controls[0].content.controls[1].controls[1].value),
-            "glucose": float(self.controls[0].content.controls[2].controls[1].value),
-            "bloodpressure": float(self.controls[0].content.controls[3].controls[1].value),
-            "skinthickness": float(self.controls[0].content.controls[4].controls[1].value),
-            "insulin": float(self.controls[0].content.controls[5].controls[1].value),
+            "gender": int(self.controls[0].content.controls[1].controls[1].value),
+            "age": float(self.controls[0].content.controls[2].controls[1].value),
+            "hypertension": int(self.controls[0].content.controls[3].controls[1].value),
+            "heart_diseases": int(self.controls[0].content.controls[4].controls[1].value),
+            "smoking_history": int(self.controls[0].content.controls[5].controls[1].value),
             "bmi": float(self.controls[0].content.controls[6].controls[1].value),
-            "diabetespedigreefunction": float(self.controls[0].content.controls[7].controls[1].value),
-            "age": int(self.controls[0].content.controls[8].controls[1].value),
+            "HbA1c_level": float(self.controls[0].content.controls[7].controls[1].value),
+            "blood_glucose_level": int(self.controls[0].content.controls[8].controls[1].value),
         }
         while True:
             try:
@@ -156,8 +89,6 @@ class MainView(ft.View):
         print(response.json()["outcome"])
         self.dlg = ft.AlertDialog(title=ft.Text(value=f'You {"Have" if response.json()["outcome"] else "Don't Have"} Diabetes!'))
         await self.open_dialog()
-
-    
 
 
 async def main(page: ft.Page):
