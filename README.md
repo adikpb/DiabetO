0077b9a262f5e100ae8b8f8ce55dbab0b737861e
adikpb <67222969+adikpb@users.noreply.github.com>
2024-02-15 16:49:55 +0530

    barebones api

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # hax-feb-15
 
-Repository to collaborate for the hackathon taking place on feb 15 in vit-ap.
+Repository to collaborate for the hackathon taking place on Feb 15, 16 in VIT-AP.
 
-Problem statement (AI): 
+_Problem statement (AI): Create a model that predicts patient health outcomes based on electronic health records_
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from . import model
+
+class Data(BaseModel):
+    pregnancies: int
+    glucose: float
+    bloodpressure: float
+    skinthickness: float
+    insulin: float
+    bmi: float
+    diabetespedigreefunction: float
+    age: int
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "hello world"}
+@app.get("/predict")
+async def predict(data: Data):
+    return int(model.predict_diabetes(**data.model_dump())[0])
